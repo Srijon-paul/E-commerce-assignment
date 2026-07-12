57ecdbe5ac51010f3bd01de9c23f4eee26671705
Supriti Jana <supritij97@gmail.com>
2026-07-13 00:29:29 +0530

    product-card search button and category filter are added

---
+++ Ecommerce-Website/api.js
@@ -0,0 +1,21 @@
+export async function getProducts() {
+
+    try {
+
+        const response = await fetch(
+            "https://fakestoreapi.com/products"
+        );
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+
+        console.log(error);
+
+        return [];
+
+    }
+
+}
